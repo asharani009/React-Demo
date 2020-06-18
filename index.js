f91df9a2f1dd51af99718500e7e57eda9f2b79cd
asharani009 <pagarwal08@gmail.com>
2020-06-18 15:53:19 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import './Demo.css';
-import App from './App';
-//import Demo from './Demo';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<div><App/></div>,document.getElementById('root'));
-serviceWorker.unregister();
